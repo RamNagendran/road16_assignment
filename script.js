e445c84e08eb172c1d4c220183c383398baadafe
ramprasath <ram.nagendran6360@gmail.com>
2024-03-13 01:37:13 +0530

    guvi-road16: callback hell countdown timer implemented

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const countdownDiv = document.getElementById("count-down-div")
+const endDiv = document.getElementById("end-div")
+const value = document.getElementById('count-value')
+
+setTimeout(() => {
+    value.innerHTML = parseInt(value.innerHTML) + 1
+    setTimeout(() => {
+        value.innerHTML = parseInt(value.innerHTML) + 1
+        setTimeout(() => {
+            value.innerHTML = parseInt(value.innerHTML) + 1
+            setTimeout(() => {
+                value.innerHTML = parseInt(value.innerHTML) + 1
+                setTimeout(() => {
+                    value.innerHTML = parseInt(value.innerHTML) + 1
+
+                    setTimeout(() => {
+                        value.innerHTML = parseInt(value.innerHTML) + 1
+                        setTimeout(() => {
+                            value.innerHTML = parseInt(value.innerHTML) + 1
+                            setTimeout(() => {
+                                value.innerHTML = parseInt(value.innerHTML) + 1
+                                setTimeout(() => {
+                                    value.innerHTML = parseInt(value.innerHTML) + 1
+                                    setTimeout(() => {
+                                        countdownDiv.style.display = 'none'
+                                        endDiv.style.display = 'flex'
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
